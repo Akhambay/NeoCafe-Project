440dedee8cfd28fc0321ab6fef3153ea4611046b
Akhambay <assyl.akhambay@gmail.com>
2024-02-05 23:19:53 +0600

    Update urls.py

--- neocafe_config/urls.py
+++ neocafe_config/urls.py
@@ -19,8 +19,8 @@ from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('users.urls')),
-    path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
-    path("api/schema/swagger/",
+    path('', include('users.urls')),
+    path("schema/", SpectacularAPIView.as_view(), name="schema"),
+    path("schema/swagger/",
          SpectacularSwaggerView.as_view(url_name="schema"), name="swagger"),
 ]
