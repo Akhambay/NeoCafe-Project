5f5d6444afec088c2b658a4885ebb1574c6e9cd9
Akhambay <assyl.akhambay@gmail.com>
2024-02-14 00:41:25 +0600

    Create Stock model

--- menu/models.py
+++ menu/models.py
@@ -1,10 +1,13 @@
 from django.db import models
 from django.utils.crypto import get_random_string
 from users.models import Branch
+from django.core.validators import MinLengthValidator
+from django.core.validators import MinValueValidator
 
 
 class Category(models.Model):
-    name = models.CharField(max_length=255, default="Выпечка")
+    name = models.CharField(max_length=100, default="Выпечка", validators=[
+                            MinLengthValidator(3)])
 
     def __str__(self):
         return f"{self.name}"
@@ -21,6 +24,10 @@ class Menu_Item(models.Model):
     branch = models.ForeignKey(
         Branch, on_delete=models.CASCADE, related_name='menu_items', blank=True, null=True
     )
+
+    current_quantity = models.PositiveIntegerField(default=100)
+    minimum_limit = models.PositiveIntegerField(default=10)
+    restock_date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return f"{self.name}"
@@ -54,3 +61,25 @@ class Ingredient(models.Model):
 
     def __str__(self):
         return f"{self.name}"
+
+
+class Stock(models.Model):
+    branch = models.ForeignKey(Branch, on_delete=models.CASCADE)
+    menu_item = models.ForeignKey(
+        Menu_Item, on_delete=models.CASCADE, null=True, blank=True)
+    ingredient = models.ForeignKey(
+        Ingredient, on_delete=models.CASCADE, null=True, blank=True)
+    current_quantity = models.PositiveIntegerField(
+        default=0, validators=[MinValueValidator(0)])
+    minimum_quantity = models.PositiveIntegerField(
+        default=10, validators=[MinValueValidator(0)])
+    restock_date = models.DateTimeField(auto_now_add=True)
+    is_enough = models.BooleanField(default=True)
+
+    def __str__(self):
+        if self.menu_item:
+            return f"Stock - {self.menu_item.name} - {self.branch.name}"
+        elif self.ingredient:
+            return f"Stock - {self.ingredient.name} - {self.branch.name}"
+        else:
+            return "Invalid Stock Entry"
