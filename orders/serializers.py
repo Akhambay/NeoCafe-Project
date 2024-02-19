ddbbab73d6508c376321e49da85e9181c54ab37b
Akhambay <assyl.akhambay@gmail.com>
2024-02-19 14:16:16 +0600

    Fix Swagger

--- orders/serializers.py
+++ orders/serializers.py
@@ -18,7 +18,7 @@ class OrderSerializer(serializers.ModelSerializer):
     class Meta:
         model = Order
         fields = ['id', 'status', 'order_type', 'created_at', 'updated_at', 'completed_at',
-                  'customer_email', 'table', 'employee', 'branch', 'items']
+                  'customer', 'table', 'employee', 'branch', 'items']
 
 
 class CustomerOrderSerializer(serializers.ModelSerializer):
