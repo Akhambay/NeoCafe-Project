1b3e01dc046ffda7107b9a2903ae200aa27c2712
Akhambay <assyl.akhambay@gmail.com>
2024-03-13 15:16:54 +0500

    Check Debug

--- neocafe_config/settings.py
+++ neocafe_config/settings.py
@@ -123,7 +123,7 @@ SPECTACULAR_SETTINGS = {
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-
+"""
 if DEBUG:
     DATABASES = {
         'default': {
@@ -143,15 +143,15 @@ else:
         }
     }
 """
-DEBUG = True
-
+# DEBUG = True
+DEBUG = False
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-"""
+
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
