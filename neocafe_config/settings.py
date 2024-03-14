13114fcd4e41e5ffcfc4629c6ea32bf5ac7b2969
Akhambay <assyl.akhambay@gmail.com>
2024-03-15 02:49:04 +0500

    Switch DB

--- neocafe_config/settings.py
+++ neocafe_config/settings.py
@@ -124,7 +124,7 @@ SPECTACULAR_SETTINGS = {
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-"""
+
 if DEBUG:
     DATABASES = {
         'default': {
@@ -151,6 +151,7 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+"""
 
 
 # Password validation
