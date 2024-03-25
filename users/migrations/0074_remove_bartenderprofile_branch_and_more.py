7630810e58501bdf7ba1f59b24f255b323109c4a
Akhambay <assyl.akhambay@gmail.com>
2024-03-25 18:15:45 +0500

    Restore file

---
+++ users/migrations/0074_remove_bartenderprofile_branch_and_more.py
@@ -0,0 +1,10 @@
+from django.db import migrations
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('users', '0073_bartenderprofile_user_type_waiterprofile_user_type'),
+    ]
+
+    operations = [
+    ]
