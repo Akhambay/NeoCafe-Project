5e100038907a1f69a940487a06c18cec0da3bd4a
Akhambay <assyl.akhambay@gmail.com>
2024-04-05 11:45:26 +0500

    Change DB option

--- neocafe_config/settings.py
+++ neocafe_config/settings.py
@@ -139,7 +139,7 @@ SPECTACULAR_SETTINGS = {
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-"""
+
 if DEBUG:
     DATABASES = {
         'default': {
@@ -166,7 +166,7 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
+"""
 
 
 # Password validation
