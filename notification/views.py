38895fe4545619fdf38997ecd970439725474e94
Akhambay <assyl.akhambay@gmail.com>
2024-04-12 11:54:23 +0500

    Fix Notification import

--- notification/views.py
+++ notification/views.py
@@ -1,14 +1,14 @@
 # views.py
 
-# from django.shortcuts import render
-# from .models import Notification
-# from rest_framework.views import APIView
-# from rest_framework.response import Response
-# from rest_framework import status
+from django.shortcuts import render
+from .models import Notification
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 
-# def notifications_page(request):
-#     #notifications = Notification.objects.filter(is_read=False, order__order_status='Готов')
-#     return render(request, 'notifications.html')
+def notifications_page(request):
+    #notifications = Notification.objects.filter(is_read=False, order__order_status='Готов')
+    return render(request, 'notifications.html')
 
 from rest_framework.views import APIView
 from rest_framework.response import Response
