0f19ba1208543796bcf2af5e276db2a07bf9a56c
Akhambay <assyl.akhambay@gmail.com>
2024-04-12 12:01:46 +0500

    Update signals.py

--- notification/signals.py
+++ notification/signals.py
@@ -9,3 +9,12 @@ def order_status_changed(sender, instance, created, **kwargs):
     if instance.order_status == "Готов":
         notification = Notification(order=instance)
         notification.save()
+
+
+@receiver(post_save, sender=Order)
+def order_status_changed(sender, instance, created, **kwargs):
+    if instance.order_status == "Готов":
+        existing_notification = Notification.objects.filter(order=instance).first()
+        if not existing_notification:
+            notification = Notification(order=instance)
+            notification.save()
