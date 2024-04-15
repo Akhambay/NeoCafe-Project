0b145e849af4de7376789fd4a60b07f1bfb2e8f3
Akhambay <assyl.akhambay@gmail.com>
2024-04-16 01:06:04 +0500

    Update asgi.py

--- neocafe_config/asgi.py
+++ neocafe_config/asgi.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
 import os
 
 from django.core.asgi import get_asgi_application
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Neocafe24.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'neocafe_config.settings')
 django_asgi_app = get_asgi_application()
 
 import notification.routing
