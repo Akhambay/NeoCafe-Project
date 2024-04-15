3221c0417a12759530751724d1a1c47b2286c047
Akhambay <assyl.akhambay@gmail.com>
2024-04-15 15:26:04 +0500

    Update settings.py

--- neocafe_config/settings.py
+++ neocafe_config/settings.py
@@ -148,15 +148,15 @@ SPECTACULAR_SETTINGS = {
 
 if DEBUG:
     DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.postgresql_psycopg2',
-            'NAME': os.getenv('DB_NAME'),
-            'USER': os.getenv('DB_USER'),
-            'PASSWORD': os.getenv('DB_PASSWORD'),
-            'HOST': os.getenv('DB_HOST'),
-            'PORT': os.getenv('DB_PORT'),
-        }
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),  # use 'db' as the host name
+        'PORT': os.getenv('DB_PORT'),
     }
+}
 else:
     DATABASES = {
         'default': {
