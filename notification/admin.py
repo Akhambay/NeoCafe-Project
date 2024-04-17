b30e3f4c89ab1a772d2732c22100fc870f0288f3
Akhambay <assyl.akhambay@gmail.com>
2024-04-18 03:25:39 +0500

    Update admin.py

--- notification/admin.py
+++ notification/admin.py
@@ -3,6 +3,6 @@ from .models import Notification
 
 @admin.register(Notification)
 class NotificationAdmin(admin.ModelAdmin):
-    list_display = ('title', 'description', 'timestamp', 'status', 'recipient')
-    fields = ('title', 'description', 'timestamp', 'status', 'recipient', 'read')
+    list_display = ('title', 'description', 'timestamp', 'status', 'recipient', 'table', 'read')
+    fields = ('title', 'description', 'timestamp', 'status', 'recipient', 'table', 'read')
     readonly_fields = ('timestamp',)
