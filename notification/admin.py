2a1ae140424a01ac4430e898fa46073683f775de
Akhambay <assyl.akhambay@gmail.com>
2024-04-18 15:16:32 +0500

    Recreate Migrations

--- notification/admin.py
+++ notification/admin.py
@@ -3,6 +3,6 @@ from .models import Notification
 
 @admin.register(Notification)
 class NotificationAdmin(admin.ModelAdmin):
-    list_display = ('title', 'description', 'timestamp', 'status', 'recipient', 'read')
-    fields = ('title', 'description', 'timestamp', 'status', 'recipient', 'read')
+    list_display = ('title', 'description', 'timestamp', 'status', 'recipient', 'read', 'table_number')
+    fields = ('title', 'description', 'timestamp', 'status', 'recipient', 'read', 'table_number')
     readonly_fields = ('timestamp',)
