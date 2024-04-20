a3f168a2790a333ace6b21f22b5fb8c8f32bf6fd
Akhambay <assyl.akhambay@gmail.com>
2024-04-20 11:57:30 +0500

    Decode serializer

--- notification/serializers.py
+++ notification/serializers.py
@@ -4,7 +4,15 @@ from .models import Notification
 
 class UserNotificationSerializer(serializers.ModelSerializer):
     timestamp = serializers.DateTimeField(format="%d.%m.%Y %H:%M", read_only=True)
+    #title = serializers.CharField(max_length=255)
+    #description = serializers.CharField(max_length=255)
 
     class Meta:
         model = Notification
         fields = '__all__'
+
+    def to_representation(self, instance):
+        ret = super().to_representation(instance)
+        ret['title'] = ret['title'].encode('utf-8').decode('unicode_escape')
+        ret['description'] = ret['description'].encode('utf-8').decode('unicode_escape')
+        return ret
